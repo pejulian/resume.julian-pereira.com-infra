34088a9771a9321ecac4aaba30212a0b99de6b34
Julian Pereira <bleushade@gmail.com>
2024-08-04 00:28:37 +0800

    Initial commit

---
+++ test/resume.julian-pereira.com-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ResumeJulianPereiraComInfra from '../lib/resume.julian-pereira.com-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/resume.julian-pereira.com-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ResumeJulianPereiraComInfra.ResumeJulianPereiraComInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
